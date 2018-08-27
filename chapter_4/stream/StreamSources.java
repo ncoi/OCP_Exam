06b32e6b372b35a3cb5999f55589f2c4dea85dd0
Nelson Camilo Orduz Illidge <norduz@konradgroup.com>
2018-08-27 16:24:13 -0600

    Chapter 4 streams

---
+++ chapter_4/stream/StreamSources.java
@@ -0,0 +1,22 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamSources {
+	public static void main(String[] args) {
+		Stream<String> empty = Stream.empty();         // count = 0
+		Stream<Integer> singleElement = Stream.of(1);  // count = 1
+		Stream<Integer> fromArray = Stream.of(1,2,3);  // count = 3
+		
+		// convert from a list to a stream
+		List<String> list = Arrays.asList("a","b","c");
+		Stream<String> fromList = list.stream();
+		Stream<String> fromListParallel = list.parallelStream();
+		
+		// infinite stream
+		Stream<Double> randoms = Stream.generate(Math::random);
+		Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
+	}
+}
