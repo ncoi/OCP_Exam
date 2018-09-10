cca95453308b3b211e0542a0175a62138b2dfc8a
Nelson Camilo Orduz Illidge <norduz@konradgroup.com>
2018-09-10 14:47:25 -0600

    stuff from chapter 4

---
+++ chapter_4/primitive_functional_interfaces/BooleanSupplierInt.java
@@ -0,0 +1,14 @@
+package primitive_functional_interfaces;
+
+import java.util.function.BooleanSupplier;
+
+public class BooleanSupplierInt {
+
+	public static void main(String[] args) {
+		BooleanSupplier b1 = () -> true;
+		BooleanSupplier b2 = () -> Math.random() > .5;
+		System.out.println(b1.getAsBoolean());
+		System.out.println(b2.getAsBoolean());
+	}
+
+}
--- chapter_4/stream/OptionalPrimitive.java
+++ chapter_4/stream/OptionalPrimitive.java
@@ -1,25 +1,39 @@
 package stream;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.OptionalDouble;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class OptionalPrimitive {
 	public static void main(String[] args) {
-		IntStream rng = IntStream.rangeClosed(1, 10);
-		OptionalDouble op = rng.average();
+//		IntStream rng = IntStream.rangeClosed(1, 10);
+//		OptionalDouble op = rng.average();
+//		
+//		op.ifPresent(System.out::println);
+//		System.out.println(op.getAsDouble());
+//		System.out.println(op.orElseGet(() -> Double.NaN));
+//		
+//		
+//		LongStream longs = LongStream.of(5,10);
+//		long sum = longs.sum();
+//		System.out.println(sum);
+//		
+//		DoubleStream doubles = DoubleStream.generate(() -> Math.PI);
+//		OptionalDouble min = doubles.min();
 		
-		op.ifPresent(System.out::println);
-		System.out.println(op.getAsDouble());
-		System.out.println(op.orElseGet(() -> Double.NaN));
+//		Stream<Integer> r = Stream.of(1,2,3,4,5);
+//		r.filter(m -> m instanceof Number)
+//		.peek(System.out::println)
+//		.sorted()
+//		.forEach(System.out::println);
 		
 		
-		LongStream longs = LongStream.of(5,10);
-		long sum = longs.sum();
-		System.out.println(sum);
-		
-		DoubleStream doubles = DoubleStream.generate(() -> Math.PI);
-		OptionalDouble min = doubles.min();
+		Duration d = Duration.between(LocalDateTime.of(2015, Month.SEPTEMBER, 2, 1, 0), LocalDateTime.of(2015, Month.SEPTEMBER, 2, 0, 30));
+		System.out.println(d);
 	}
 }
